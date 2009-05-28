088c732a08965ba40c5e5414748f7a5e02a76adf
Jan Kubr <jan.kubr@gmail.com>
2009-05-28 09:35:52 +0200

    init

---
+++ freeswitch_socket.rb
@@ -0,0 +1,23 @@
+class FreeswitchSocket
+  
+  def initialize
+    @socket = TCPSocket.open("#{FREESWITCH_HOST}", 8021)
+    authenticate
+  end
+
+private  
+
+  def authenticate
+    @socket.gets
+    @socket.gets
+    command("auth #{FREESWITCH_PASSWORD}")
+  end
+  
+  def command(cmd)
+    @socket.send("#{cmd}\n\n", 1024)
+    @socket.gets
+    @socket.gets
+    @socket.gets        
+  end
+  
+end
